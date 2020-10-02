8d8fc8a406bf3667ec3ab7ac3352cbbf46dfdd59
Ellaqyzeng <46781793+Ellaqyzeng@users.noreply.github.com>
2020-10-02 02:47:19 +0100

    Add files via upload

---
+++ Week4/ui.R
@@ -0,0 +1,28 @@
+# Developing Data Products_Week 4
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  headerPanel("Interactive Statistical Distribution"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarPanel(
+    numericInput("n", "Generate this many points", 
+                 min = 1, value = 500),
+    selectInput("family", "From this family",
+                choices = c("Normal",
+                            "Uniform",
+                            "Exponential"),
+                selected = "normal"),
+    sliderInput("bins", "number of bins", 
+                min = 1, max = 50, value = 30)
+  ),
+  
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("histogram")
+  )
+)
